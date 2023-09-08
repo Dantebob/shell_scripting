f2642a235c68f0bf7497ddb8d8a9d64987f7ec7a
Dantebob <dantecelani@gmail.com>
2023-09-08 15:34:19 -0600

    uses 2 python scripts. Finished?

--- count_bp.py
+++ count_bp.py
@@ -5,22 +5,19 @@ import os
 def count_bp(path):
     #get then read fasta files
     dirs = os.listdir(path)
-    print(len(dirs))
     fasta_list = []
     for d in dirs:
         if ".fasta" in d:
             fasta_list.append(d)
     
-    print(len(fasta_list))
     count = 0
     #read sequence in each fasta
     for f in fasta_list:
         fasta_path = path + f
-        print(f)
         with open(fasta_path, "r") as fasta:
             lines = fasta.readlines()
             # lines[0] is sample lines[1] is the sequence
-            if lines[1]:
+            if len(lines) > 1:
                 count += len(lines[1])
     return count
 
--- make_log.sh
+++ make_log.sh
@@ -4,17 +4,19 @@ cd=~/shell_scripting/
 
 # python script puts sequences in individual fasta files named after sample and puts them in individual_fastas
 python3 make_fasta_per_seq.py
+bp_num=$(python3 count_bp.py 2> /dev/null)
 #python3 count_bp.py
 num_fasta_files=$( ls ${cd}/ExampleAlignments/ | grep -c "\.fasta" )
 num_sequences=$( ls ${cd}/individual_fastas/ | grep -c "\.fasta" )
-num_bp=$( cat ${cd}/individual_fastas/Sample* | grep -v "^>" | wc -c )
+#num_bp=$( cat ${cd}/individual_fastas/Sample* | grep -v "^>" | wc -c )
 
 #clear log then append to it
 echo "" > log.txt
 echo $num_fasta_files >> log.txt
 echo $num_sequences >> log.txt
-echo $num_bp >> log.txt
-echo "$num_fasta_files"
-echo "$num_sequences"
-echo "BP $num_bp"
+echo $bp_num >> log.txt
+
+echo "Number of fasta files: $num_fasta_files"
+echo "Number of sequences: $num_sequences"
+echo "Number of base pairs: $bp_num"
 
