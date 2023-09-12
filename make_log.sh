76e97a468bd33ea0ff4f8da2d4095a8682ea9d3f
dantebob <dantecelani@gmail.com>
2023-09-12 16:24:39 -0600

    changed working directory for make_log.sh

--- make_log.sh
+++ make_log.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-cd=~/shell_scripting/
+wd=~/Scripting_for_biologists/shell_scripting/
 
 # python script puts sequences in individual fasta files named after sample and puts them in individual_fastas
 python3 make_fasta_per_seq.py
 bp_num=$(python3 count_bp.py 2> /dev/null)
 #python3 count_bp.py
-num_fasta_files=$( ls ${cd}/ExampleAlignments/ | grep -c "\.fasta" )
-num_sequences=$( ls ${cd}/individual_fastas/ | grep -c "\.fasta" )
+num_fasta_files=$( ls ${wd}/ExampleAlignments/ | grep -c "\.fasta" )
+num_sequences=$( ls ${wd}/individual_fastas/ | grep -c "\.fasta" )
 #num_bp=$( cat ${cd}/individual_fastas/Sample* | grep -v "^>" | wc -c )
 
 #clear log then append to it
